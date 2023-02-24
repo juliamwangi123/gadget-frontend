f2b7764db122931c70b42fb63f4bfa9ab097b4aa
Olliemint <furywrld5@gmail.com>
2023-02-24 12:48:57 +0300

    refactor: updated urls endpoints

--- src/Components/Auth/Signin.jsx
+++ src/Components/Auth/Signin.jsx
@@ -5,7 +5,7 @@ import logo from "../subcomponents/assets/logo.png"
 const Signin = () => {
   return (
     <div className="w-full mx-auto max-w-7xl">
-      <NavLink to="/home" className="">
+      <NavLink to="/" className="">
         <img className="w-[5.5rem] h-[4.2rem]" src={logo} alt="" />
       </NavLink>
       <h1> Signin up page</h1>
--- src/Components/Auth/Signup.jsx
+++ src/Components/Auth/Signup.jsx
@@ -7,7 +7,7 @@ import logo from "../subcomponents/assets/logo.png";
 const Signup = () => {
   return (
     <div className="w-full mx-auto max-w-7xl">
-      <NavLink to="/home" className="">
+      <NavLink to="/" className="">
         <img className="w-[5.5rem] h-[4.2rem]" src={logo} alt="" />
       </NavLink>
 
