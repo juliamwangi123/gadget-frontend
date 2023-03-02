1310e903ebcc080381885fe18fa5c67e430b298f
Olliemint <furywrld5@gmail.com>
2023-03-03 01:29:28 +0300

    feat: payment icons

--- src/Components/CheckoutDetails.jsx
+++ src/Components/CheckoutDetails.jsx
@@ -15,14 +15,16 @@ const CheckoutDetails = () => {
       {/* Checkout details section */}
       <div className="mt-4 max-w-7xl mx-auto">
         <div className="flex justify-center items-center text-[#666666] gap-6">
-          <RiSecurePaymentFill size={50}  className=""/>
-          <div className='max-w-[100px] leading-5 capitalize text-[15px] tracking-wide'>
-            <p>secure  payment</p>
+          <RiSecurePaymentFill size={50} className="" />
+          <div className="max-w-[100px] leading-5 capitalize text-[15px] tracking-wide">
+            <p>secure payment</p>
           </div>
         </div>
         <div className="flex flex-col-reverse sm:gap-20  sm:flex-row mt-4 sm:mt-12">
           <div className="">
-            <h1 className="uppercase text-[20px] font-medium tracking-wider text-center sm:text-start">Checkout</h1>
+            <h1 className="uppercase text-[20px] font-medium tracking-wider text-center sm:text-start">
+              Checkout
+            </h1>
             <div className="max-w-[838px] bg-[#F2F9FF] w-full rounded-md shadow-sm border border-stone-100 mt-4 sm:mt-6 p-4 sm:px-8">
               <div className="flex justify-between sm:mt-2">
                 <h1 className="text-sm sm:text-[20px] font-medium ">
@@ -71,10 +73,36 @@ const CheckoutDetails = () => {
                   Pay with cards
                 </label>
               </div>
+              <div className="flex gap-4 items-center mt-4 sm:mt-8 sm:pb-4 ">
+                <div className="w-[56px] border border-blue-600 h-[40px] rounded-md flex items-center justify-center">
+                  <img
+                    src="https://res.cloudinary.com/dq4fj9ops/image/upload/v1677795043/Images/Mcard_trunic.png"
+                    alt=""
+                    className="max-w-[40px]"
+                  />
+                </div>
+                <img
+                  src="https://res.cloudinary.com/dq4fj9ops/image/upload/v1677795044/Images/V_crm51e.png"
+                  alt=""
+                  className="max-w-[40px] max-h-[40px]"
+                />
+                <img
+                  src="https://res.cloudinary.com/dq4fj9ops/image/upload/v1677795044/Images/VERVE_qtsclq.png"
+                  alt=""
+                  className="max-w-[83px] max-h-[40px]"
+                />
+                <img
+                  src="https://res.cloudinary.com/dq4fj9ops/image/upload/v1677795043/Images/PAY_ucmxyx.png"
+                  alt=""
+                  className="max-w-[112.76px] max-h-[40px]"
+                />
+              </div>
             </div>
           </div>
           <div className="">
-            <h1 className="uppercase text-[20px] font-medium tracking-wider text-center sm:text-start">order summary</h1>
+            <h1 className="uppercase text-[20px] font-medium tracking-wider text-center sm:text-start">
+              order summary
+            </h1>
 
             <div className="max-w-[446px] bg-[#F2F9FF] w-full rounded-md shadow-sm border border-stone-100 p-6 sm:px-10 mt-6">
               <h1 className="sm:text-[21.505px] text-[18px] font-medium tracking-wider">
@@ -97,14 +125,18 @@ const CheckoutDetails = () => {
               </div>
             </div>
             <div className="mt-4 sm:mt-8 flex justify-center">
-              <Link to="/payment-details" className="py-2.5 px-8 text-[15px] sm:w-full uppercase text-white font-bold rounded-md sm:text-[20px] bg-[#0043C6] hover:bg-black" type="submit">
+              <Link
+                to="/payment-details"
+                className="py-2.5 px-8 text-[15px] sm:w-full uppercase text-white font-bold rounded-md sm:text-[20px] bg-[#0043C6] hover:bg-black"
+                type="submit"
+              >
                 confirm order
               </Link>
             </div>
           </div>
         </div>
       </div>
-      <div className="">
+      <div className="pt-8 ">
         <Footer />
       </div>
     </div>
