76e36a1352079dcbbbb76a7f6ae0f6a06e6b7a0c
Olliemint <furywrld5@gmail.com>
2023-03-03 01:05:36 +0300

    fix: fixed route mismatch

--- src/App.jsx
+++ src/App.jsx
@@ -26,8 +26,8 @@ function App() {
     <div className="">
       <Routes>
         <Route exact path="/" element={<Home />} />
-        <Route path="/login" element={<Signin />} />
-        <Route path="/register" element={<Signup />} />
+        <Route path="/account/login" element={<Signin />} />
+        <Route path="/account/register" element={<Signup />} />
         <Route path="/sell-gadget" element={<SellGadget />} />
         <Route path="/product-details" element={<SellDetails />} />
         <Route path="/customers-feedback" element={<FeedbackForm />} />
