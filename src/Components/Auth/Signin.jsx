1eef941a1c91506048c056eb802c960322bd5815
Olliemint <furywrld5@gmail.com>
2023-03-06 16:09:49 +0300

    refactor & styling: improved sign up and login page and added floating labels

--- src/Components/Auth/Signin.jsx
+++ src/Components/Auth/Signin.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom';
+import { MdVisibilityOff } from "react-icons/md";
+
 import logo from "../subcomponents/assets/logo.png"
 
 const Signin = () => {
@@ -9,62 +11,100 @@ const Signin = () => {
         <NavLink to="/" className="">
           <img className="w-[5.5rem] h-[4.2rem]" src={logo} alt="" />
         </NavLink>
-        <h1 className=" m-auto text-center font-medium text-2xl ">
+        <h1 className=" m-auto text-center font-medium text-[24px] tracking-wide">
           Login
         </h1>
       </nav>
-      <section>
-      <div className=" bg-[#F2F9FF]  rounded-[10px] w-2/3 md:w-1/2 mx-auto my-10">
-        <div className=" flex flex-wrap p-7 justify-between mx-auto">
-          <img
-            src="https://res.cloudinary.com/eiroro/image/upload/v1677002782/Group_374_lbpcjg.svg"
-            alt="Sign up with Google"
-            className=" mb-4 md:mb-0"
-          />
-          <img
-            src="https://res.cloudinary.com/eiroro/image/upload/v1677002782/Group_375_xlfcsi.svg"
-            alt="Sign up with Facebook"
-            className=" mb-4 md:mb-0"
-          />
-        </div>
-        <form className=" p-6">
-          <input
-            className=" rounded-[10px] bg-transparent border border-[#0000001A] text-sm p-5 w-full mb-4 "
-            type="email"
-            name="email"
-            placeholder="Email address"
-          />
-          <input
-            className=" rounded-[10px] bg-transparent border border-[#0000001A] text-sm p-5 w-full mb-4 "
-            type="password"
-            name="password"
-            placeholder="Password"
-          />
-
-          <input
-            className=" bg-[#0043C6] rounded-[10px] w-full p-[10px] my-5 text-white font-bold"
-            type="submit"
-            value="Login"
-          />
-          <div className=" my-5">
-            <input type="checkbox" id="remember" name="T&Cs" value="Remember" />
-            <label for="Remember me" className=" text-[#00000094]">
-              {" "}
-              Remember Me
-            </label>
-            <p className=" inline float-right text-[#FF0000CC]">
-              {" "}
-              Forgot Your Password
-            </p>
+      <section className="flex justify-center items-center mt-4 sm:mt-6">
+        <div className=" bg-[#F2F9FF] rounded-[10px] max-w-[639px] w-full p-6 sm:p-8 md:p-10">
+          <div className=" flex flex-wrap justify-between">
+            <img
+              src="https://res.cloudinary.com/eiroro/image/upload/v1677002782/Group_374_lbpcjg.svg"
+              alt="Sign up with Google"
+              className=" mb-4 md:mb-0"
+            />
+            <img
+              src="https://res.cloudinary.com/eiroro/image/upload/v1677002782/Group_375_xlfcsi.svg"
+              alt="Sign up with Facebook"
+              className=" mb-4 md:mb-0"
+            />
           </div>
-          <p className=" text-[#00000094] text-center mb-8">
-            {" "}
-            Don't have an account?{" "}
-            <NavLink to="/account/register" className=" text-[#FF0000CC]">Sign Up</NavLink>
-          </p>
-        </form>
-      </div>
-    </section>
+          <div className="flex justify-center my-4 sm:my-8">
+            <p className="text-[20px] uppercase font-light">-or-</p>
+          </div>
+          <form className="flex flex-col gap-6 sm:gap-8 md:gap-10">
+            <div className="relative">
+              <input
+                className="peer rounded-[10px] bg-transparent border border-[#ADADAD] py-3  placeholder:text-[14px]  w-full max-w-[553px] placeholder:font-medium px-4 sm:px-6 placeholder:text-[#F2F9FF] focus:border-none "
+                type="text"
+                name="name"
+                placeholder="Email Address"
+              />
+              <label
+                className="absolute pointer-events-none left-0 peer-placeholder-shown:scale-100 duration-300 -top-[10.5px] translate-x-4 text-[#A3A3A3] font-medium text-sm peer-placeholder-shown:text-[14px] peer-placeholder-shown:top-4 bg-[#F2F9FF] px-2 peer-focus:-top-[10.5px] peer-focus:text-sm peer-focus:text-blue-500"
+                htmlFor=""
+              >
+                Email address
+              </label>
+            </div>
+            <div className=" relative flex items-center">
+              <MdVisibilityOff
+                size={25}
+                className="absolute right-0 mr-6 text-[#A3A3A3]"
+              />
+              <input
+                className="peer rounded-[10px] bg-transparent border border-[#ADADAD] py-3 placeholder:text-[#A3A3A3] placeholder:text-[14px]  w-full max-w-[553px] placeholder:font-medium px-4 sm:px-6 focus:border-none  placeholder-transparent"
+                type="text"
+                name="name"
+                placeholder=" "
+              />
+              <label
+                className="absolute translate-x-4 -top-3   peer-placeholder-shown:top-3.5 peer-placeholder-shown:translate-x-4 peer-placeholder-shown:scale-100 duration-300 pointer-events-none peer-focus:-top-3.5 peer-focus:text-blue-500 bg-[#F2F9FF] px-1 text-[#A3A3A3]"
+                htmlFor=""
+              >
+                Password
+              </label>
+            </div>
+
+            <div className="w-full max-w-[551px] flex flex-col">
+              <button
+                className="bg-[#0043C6] rounded-[10px] text-[20px] capitalize text-white py-2.5 font-bold tracking-wide  grow hover:bg-black"
+                type="submit"
+              >
+                login
+              </button>
+            </div>
+            <div className="flex items-center justify-between">
+              <div className="flex gap-4 items-center">
+                <input
+                  type="checkbox"
+                  id="remember"
+                  name="T&Cs"
+                  value="Remember"
+                />
+                <label
+                  for="Remember me"
+                  className=" text-[#00000094] text-[16px] font-medium"
+                >
+                  {" "}
+                  Remember Me
+                </label>
+              </div>
+              <p className="text-[15px] font-medium text-[#FF0000CC] mr-2">
+                {" "}
+                Forgot Your Password ?
+              </p>
+            </div>
+            <p className=" text-[#00000094] text-center mb-8 text-[15px] font-medium tracking-wide md:pb-6">
+              {" "}
+              Don't have an account?{" "}
+              <NavLink to="/account/register" className=" text-[#FF0000CC]">
+                Sign Up
+              </NavLink>
+            </p>
+          </form>
+        </div>
+      </section>
     </div>
   );
 }
--- src/Components/Auth/Signup.jsx
+++ src/Components/Auth/Signup.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-
+import {MdVisibilityOff} from "react-icons/md"
 import logo from "../subcomponents/assets/logo.png";
 
 const Signup = () => {
@@ -10,16 +10,16 @@ const Signup = () => {
         <NavLink to="/" className="">
           <img className="w-[5.5rem] h-[4.2rem]" src={logo} alt="" />
         </NavLink>
-        <h1 className=" m-auto text-center font-medium text-2xl ">
+        <h1 className=" m-auto text-center font-medium text-[24px] leading-[34px] tracking-wide">
           Create Your Account
         </h1>
       </nav>
 
-      <section>
-        <div className=" flex justify-between  ">
-          <div className=" md:w-1/2 p-6">
-            <div className=" bg-[#F2F9FF]  rounded-[10px]">
-              <div className=" flex flex-wrap p-7 justify-between">
+      <section className="mt-4 sm:mt-6 md:mt-8 sm:ml-6 md:ml-8">
+        <div className="flex justify-between items-center">
+          <div className="w-full max-w-[639px]">
+            <div className="bg-[#F2F9FF]  rounded-[10px] p-8 sm:p-10 md:p-12">
+              <div className=" flex flex-wrap justify-center sm:justify-between">
                 <img
                   src="https://res.cloudinary.com/eiroro/image/upload/v1677002782/Group_374_lbpcjg.svg"
                   alt="Sign up with Google"
@@ -31,60 +31,118 @@ const Signup = () => {
                   className=" mb-4 md:mb-0"
                 />
               </div>
-              <h2 className=" text-center text-xl font-normal">-OR-</h2>
-              <form className=" p-6">
-                <input
-                  className=" rounded-[10px] bg-transparent border border-[#0000001A] text-sm p-5 w-full mb-4 "
-                  type="text"
-                  name="name"
-                  placeholder="Full Name*"
-                />
-                <input
-                  className=" rounded-[10px] bg-transparent border border-[#0000001A] text-sm p-5 w-full mb-4 "
-                  type="email"
-                  name="email"
-                  placeholder="Email address"
-                />
-                <input
-                  className=" rounded-[10px] bg-transparent border border-[#0000001A] text-sm p-5 w-full mb-4 "
-                  type="password"
-                  name="password"
-                  placeholder="Password"
-                />
-                <input
-                  className=" rounded-[10px] bg-transparent border border-[#0000001A] text-sm p-5 w-full mb-4 "
-                  type="password"
-                  name="password"
-                  placeholder="Confirm Password"
-                />
-                <input
-                  type="checkbox"
-                  name="T&Cs"
-                  value="Accept"
-                  required
-                />
-                <label for="consent" className=" text-[#00000094]">
-                  {" "}
-                  I read and consent to the{" "}
-                  <span className=" text-[#FF0000CC]">
-                    Terms and Conditions
-                  </span>
-                </label>
+              <h2 className=" text-center text-[20px] font-light my-4 sm:my-8">
+                -OR-
+              </h2>
+              <form className="">
+                <div className="flex flex-col gap-4 sm:gap-8  md:gap-10">
+                  <div className="relative">
+                    <input
+                      className="peer focus:border-none rounded-[10px] bg-transparent border border-[#ADADAD] py-3 placeholder:text-[#A3A3A3] placeholder:text-[14px]  w-full max-w-[553px] placeholder:font-medium px-4 sm:px-6"
+                      type="text"
+                      name="name"
+                      placeholder=" "
+                    />
+                    <label
+                      className="absolute text-[#A3A3A3] left-0 scale-100 duration-300 capitalize translate-x-4 px-3 tracking-wide -top-3 bg-[#F2F9FF] peer-placeholder-shown:top-3.5 peer-focus:-top-3 pointer-events-none peer-focus:text-blue-500"
+                      htmlFor=""
+                    >
+                      full name <span className="absolute top-1">*</span>
+                    </label>
+                  </div>
+                  <div className="relative">
+                    <input
+                      className="peer focus:border-none rounded-[10px] bg-transparent border border-[#ADADAD] py-3 placeholder:text-[#A3A3A3] placeholder:text-[14px]  w-full max-w-[553px] placeholder:font-medium px-4 sm:px-6"
+                      type="text"
+                      name="name"
+                      placeholder=" "
+                    />
+                    <label
+                      className="absolute text-[#A3A3A3] left-0 scale-100 duration-300 capitalize translate-x-4 px-3 tracking-wide -top-3 bg-[#F2F9FF] peer-placeholder-shown:top-3.5 peer-focus:-top-3 pointer-events-none peer-focus:text-blue-500 "
+                      htmlFor=""
+                    >
+                      email address
+                    </label>
+                  </div>
+                  <div className=" relative flex items-center">
+                    <MdVisibilityOff
+                      size={25}
+                      className="absolute right-0 mr-5 text-[#A3A3A3]"
+                    />
+                    <input
+                      className="peer rounded-[10px] bg-transparent border border-[#ADADAD] py-3 placeholder:text-[#A3A3A3] placeholder:text-[14px]  w-full max-w-[553px] placeholder:font-medium px-4 sm:px-6 focus:border-none"
+                      type="text"
+                      name="name"
+                      placeholder=" "
+                    />
+                    <label
+                      className="absolute px-2 translate-x-4 capitalize text-[#A3A3A3] -top-3  tracking-wide bg-[#F2F9FF] scale-100 duration-300 peer-placeholder-shown:top-3 pointer-events-none peer-focus:-top-3 peer-focus:text-blue-500"
+                      htmlFor=""
+                    >
+                      password
+                    </label>
+                  </div>
+                  <div className=" relative flex items-center">
+                    <MdVisibilityOff
+                      size={25}
+                      className="absolute right-0 mr-5 text-[#A3A3A3]"
+                    />
+                    <input
+                      className="peer rounded-[10px] bg-transparent border border-[#ADADAD] py-3 placeholder:text-[#A3A3A3] placeholder:text-[14px]  w-full max-w-[553px] placeholder:font-medium px-4 sm:px-6"
+                      type="text"
+                      name="name"
+                      placeholder=" "
+                    />
+                    <label
+                      className="absolute px-2 translate-x-4 capitalize tracking-wide text-[#A3A3A3] -top-3 bg-[#F2F9FF] scale-100 duration-300 peer-placeholder-shown:top-3 peer-focus:-top-3 pointer-events-none peer-focus:text-blue-500"
+                      htmlFor=""
+                    >
+                      confirm password
+                    </label>
+                  </div>
+                </div>
+                <div className="flex gap-4 items-center mt-4 sm:mt-8 md:mt-10">
+                  <input
+                    type="checkbox"
+                    name="T&Cs"
+                    value="Accept"
+                    required
+                    className="w-6 h-6 bg-transparent"
+                  />
+                  <label
+                    for="consent"
+                    className=" text-[#00000094] font-medium"
+                  >
+                    {" "}
+                    I Read and Consent to the{" "}
+                    <span className=" text-[#FF0000CC]">
+                      Terms and Conditions
+                    </span>
+                  </label>
+                </div>
 
-                <input
-                  className=" bg-[#0043C6] rounded-[10px] w-full p-[10px] my-5 text-white font-bold"
-                  type="submit"
-                  value="Create Account"
-                />
-                <p className=" text-[#00000094]">
+                <div className="w-full max-w-[551px] flex flex-col mt-4 sm:mt-6 md:mt-8">
+                  <button
+                    className="bg-[#0043C6] rounded-[10px] text-[20px] capitalize text-white py-3 font-bold tracking-wide  grow hover:bg-black"
+                    type="submit"
+                  >
+                    create account
+                  </button>
+                </div>
+                <p className=" text-[#00000094] mt-4 sm:mt-6 md:mt-8 text-[15px] tracking-wide pb-2 sm:pb-4">
                   {" "}
                   Already have an account?{" "}
-                  <NavLink to="/account/login" className=" text-[#FF0000CC]">Login</NavLink>
+                  <NavLink
+                    to="/account/login"
+                    className=" text-[#FF0000CC] font-semibold tracking-wide text-[15px]"
+                  >
+                    Login
+                  </NavLink>
                 </p>
               </form>
             </div>
           </div>
-          <div className=" hidden md:block m-auto h-fit  ">
+          <div className=" hidden md:block m-auto h-fit">
             <img
               src="https://res.cloudinary.com/eiroro/image/upload/v1677002783/Group_373_qmepoo.svg"
               alt="vector"
